5be8c4cea837749029117e46e3f59ea58654194d
mo2men <momen.elsyd@gmail.com>
2025-04-22 20:44:27 +0200

    first commit

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,12 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  // Allow self-signed SSL certificates if needed
+    }
+  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
