25aa18d85b3c373a7ad2ba5b9538f47c69317d8d
mo2men <momen.elsyd@gmail.com>
2025-04-22 20:40:07 +0200

    first commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "0",
-  DB: "biddo_postgres",
+  HOST: "c952v5ogavqpah.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
+  USER: "u2nb35lh4k5f5o",
+  PASSWORD: "pc3eccc92081f01e7565d5d6d286cec1d00d5642b0b501d0e92de188f3216ac0c",
+  DB: "dehiedhnehs737",
   dialect: "postgres",
   pool: {
     max: 5,
